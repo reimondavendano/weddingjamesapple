3e3a91ba067f016a0eb658808a93d663d4e19993
Reimond Avendano <reimondavendano@gmail.com>
2024-11-20 14:42:40 +0800

    Add preloader

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -3,6 +3,23 @@ import '../assets/styles/countdown.css'; // Import your custom CSS for styling
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Countdown = ({ targetDate }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+      const handleScroll = () => {
+        const section = document.querySelector('.countdown-section');
+        const sectionTop = section.getBoundingClientRect().top;
+        const viewportHeight = window.innerHeight;
+        
+        if (sectionTop < viewportHeight * 0.75) {
+          setIsVisible(true);
+        }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+      return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     const calculateTimeLeft = () => {
       const difference = +new Date(targetDate) - +new Date();
       let timeLeft = {};
@@ -30,41 +47,43 @@ const Countdown = ({ targetDate }) => {
     }, []);
   
     return (
-      <Container fluid>
-        <Row className="justify-content-center">
-            
-            <Col md={12}>
-            <div className="countdown-container">
-                <Row>
-                    
-                    <Col md={6}>
-                    <p className='font-agradir text-align-center countdown-p  font-size-20 white-color '>We are waiting for....</p>
-                    <p className='font-madelyn text-align-center countdown-p  font-size-50 white-color '>The Adventure</p>
-                    </Col>
-                    <Col md={6} className='justify-content-center'>
-                       
-                        {Object.keys(timeLeft).length ? (
-                        <div className="countdown">
-                            {Object.entries(timeLeft).map(([unit, value]) => (
-                            <div key={unit} className="time-box">
-                                <div className="time-value">
-                                {String(value).padStart(2, "0")}
-                                </div>
-                                <div className="time-unit">{unit}</div>
-                            </div>
-                            ))}
-                        </div>
-                        ) : (
-                        <div className="celebration">It's the big day!</div>
-                        )}
-                
-                    </Col>
-                </Row>
-                </div>
-            </Col>
-            
-        </Row>
-      </Container>
+      <section className={`countdown-section ${isVisible ? 'visible' : ''}`}>
+        <Container fluid>
+          <Row className="justify-content-center">
+              
+              <Col md={12}>
+              <div className="countdown-container">
+                  <Row>
+                      
+                      <Col md={6}>
+                      <p className='font-agradir text-align-center countdown-p  font-size-20 white-color '>We are waiting for....</p>
+                      <p className='font-madelyn text-align-center countdown-p  font-size-50 white-color '>The Adventure</p>
+                      </Col>
+                      <Col md={6} className='justify-content-center'>
+                        
+                          {Object.keys(timeLeft).length ? (
+                          <div className="countdown">
+                              {Object.entries(timeLeft).map(([unit, value]) => (
+                              <div key={unit} className="time-box">
+                                  <div className="time-value">
+                                  {String(value).padStart(2, "0")}
+                                  </div>
+                                  <div className="time-unit">{unit}</div>
+                              </div>
+                              ))}
+                          </div>
+                          ) : (
+                          <div className="celebration">It's the big day!</div>
+                          )}
+                  
+                      </Col>
+                  </Row>
+                  </div>
+              </Col>
+              
+          </Row>
+        </Container>
+      </section>
       
     );
   };
